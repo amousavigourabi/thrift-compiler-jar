7f2b9260e55715f067bd397ed356e31725a7d71e
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-07-12 16:51:47 +0200

    Makes custom extraction exception an IOException

--- src/main/java/me/atour/thriftjar/CannotExtractExecutableException.java
+++ src/main/java/me/atour/thriftjar/CannotExtractExecutableException.java
@@ -1,6 +1,8 @@
 package me.atour.thriftjar;
 
+import java.io.IOException;
+
 /**
  * Thrown when the executable cannot be extracted.
  */
-public class CannotExtractExecutableException extends RuntimeException {}
+public class CannotExtractExecutableException extends IOException {}
